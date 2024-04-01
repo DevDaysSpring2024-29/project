cf622184a6b02b391f7d77f910078bd4721bf156
shishqa <shishqa.main@gmail.com>
2024-04-01 22:55:45 +0300

    feat: add kinopoisk provider

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -2,10 +2,12 @@ import typing
 
 from . import interface
 from .dummy import provider as dummy
+from .kinopoisk import provider as kinopoisk
 
 
 async def get_providers(config: typing.Dict[str, str]) -> typing.Dict[interface.ProviderKind, interface.ProviderInterface]:
 
     return {
-        interface.ProviderKind.DUMMY: await dummy.get_provider(config)
+        interface.ProviderKind.DUMMY: await dummy.get_provider(config),
+        interface.ProviderKind.KINOPOISK: await kinopoisk.get_provider(config),
     }
---
+++ src/providers/kinopoisk/provider.py
@@ -0,0 +1,35 @@
+import typing
+
+import httpx
+
+from models import entry
+from providers import interface as providers
+
+
+class KinopoiskProvider(providers.ProviderInterface):
+    def __init__(self, token: str):
+        self.token = token
+
+    async def get_entries(self, params: providers.ProviderParams) -> list[entry.ProviderEntry]:
+        headers = {
+            'x-api-key': self.token,
+        }
+
+        async with httpx.AsyncClient() as client:
+            r = await client.get('https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=JANUARY', headers=headers)
+
+            data = r.json()
+            return [
+                entry.ProviderEntry(
+                    name=item["nameRu"],
+                    descr=None,
+                    rating=None,
+                    price=None,
+                    picture_url=None,
+                )
+                for item in data["items"]
+            ]
+
+
+async def get_provider(config: typing.Dict[str, str]) -> providers.ProviderInterface:
+    return KinopoiskProvider(config["KINOPOISK_TOKEN"])
