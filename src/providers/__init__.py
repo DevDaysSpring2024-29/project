56ec8fca5b67c8a6ea1b6d77196f5a36a3b743a5
san-kir-k <dfomka@list.ru>
2024-04-02 01:16:16 +0300

    Add basic restaurants provider

--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -3,6 +3,7 @@ import typing
 from . import interface
 from .dummy import provider as dummy
 from .kinopoisk import provider as kinopoisk
+from .restaurants import provider as restaurants
 
 
 async def get_providers(config: typing.Dict[str, str]) -> typing.Dict[interface.ProviderKind, interface.ProviderInterface]:
@@ -10,4 +11,5 @@ async def get_providers(config: typing.Dict[str, str]) -> typing.Dict[interface.
     return {
         interface.ProviderKind.DUMMY: await dummy.get_provider(config),
         interface.ProviderKind.KINOPOISK: await kinopoisk.get_provider(config),
+        interface.ProviderKind.RESTAURANTS: await restaurants.get_provider(config),
     }
