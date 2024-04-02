e834900dcf913299315dbc8a0db4d7434b7e7076
achains <arthur.salio@gmail.com>
2024-04-02 22:16:01 +0300

    feat: base bot structure

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,15 @@
 import dotenv
+import os
 
 from quo_bot import QuoBot
 
 
 def main():
-    bot = QuoBot("hey")
+    config = {
+        **dotenv.dotenv_values(".env"),
+        **os.environ,
+    }
+    bot = QuoBot(config["BOT_TOKEN"])
 
 
 if __name__ == "__main__":
--- src/quo_bot.py
+++ src/quo_bot.py
@@ -1,6 +1,12 @@
 import telegram
+import functools
 
+import handler_type
+
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Application, ContextTypes
+from telegram.ext import CommandHandler, MessageHandler
+from telegram.ext import filters
 
 __all__ = ["QuoBot"]
 
@@ -20,7 +26,46 @@ class QuoBot:
             self.__initialized = True
             self.__token = token
             self.__app = Application.builder().token(self.__token).build()
+            self._setup_handlers()
 
+            self.__button_map = {
+                "start": {"Host room": self.host_room, "Join room": self.join_room},
+            }
 
+            self.__app.run_polling()
+
+    def _setup_handlers(self):
+        for handler_name in filter(lambda n: not n.startswith("_"), dir(self)):
+            handler = getattr(self, handler_name)
+            if callable(handler) and getattr(handler, "is_command", False):
+                self.__app.add_handler(CommandHandler(handler_name, handler))
+
+        button_reply_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, self._handle_buttons)
+        self.__app.add_handler(button_reply_handler)
+
+    async def _handle_buttons(self, update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        message_to_handler = functools.reduce(lambda acc, d: acc | d, self.__button_map.values())
+        message = update.message.text
+        if message in message_to_handler:
+            await message_to_handler[message](update, context)
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="There is no such button option!")
+
+    @handler_type.command
     async def start(self, update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        buttons = list(self.__button_map["start"].keys())
+        reply_markup = ReplyKeyboardMarkup(buttons, one_time_keyboard=True, resize_keyboard=True)
+
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="TODO: Greetings",
+                                       reply_markup=reply_markup)
+
+    @handler_type.button
+    async def join_room(self, update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        print("join room")
+        pass
+
+    @handler_type.button
+    async def host_room(self, update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+        print("host room")
         pass
