8dd09c635bb8a63cac12dff80de6a86fbd3708a7
san-kir-k <dfomka@list.ru>
2024-04-04 21:45:44 +0300

    Add reference

--- src/providers/city/provider.py
+++ src/providers/city/provider.py
@@ -11,22 +11,22 @@ class CityProvider(providers.ProviderInterface):
         self.overpass_url = overpass_url
         self.query = ('/* Get list of cities in Russian. */'
                       "[out:json];area[name='Россия']->.russia;(node[place=city](area.russia););out 30;")
+        self.ref_template = "https://maps.yandex.ru/?text={lat}+{lng}"
 
     async def get_entries(self, params: providers.ProviderParams) -> list[entry.ProviderEntry]:
 
         async with httpx.AsyncClient() as client:
             r = await client.get(self.overpass_url, params={'data': self.query}, timeout=None)
 
-            data = [element['tags']["name:ru"] for element in r.json()['elements']]
             return [
                 entry.ProviderEntry(
-                    name=element,
-                    descr=None,
+                    name=element['tags']["name:ru"],
+                    descr=f"На карте: {self.ref_template.format(lat=element['lat'], lng=element['lon'])}",
                     rating=None,
                     price=None,
                     picture_url=None,
                 )
-                for element in data
+                for element in r.json()['elements']
             ]
 
 
